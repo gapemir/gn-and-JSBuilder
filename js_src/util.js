354d118b8ed5b484b889ba41ce1d8195ebd17b8a
Gašper Nemgar <gasper.nemgar@gmail.com>
2026-09-13 21:27:17 +0200

    uuid helper

--- js_src/util.js
+++ js_src/util.js
@@ -48,4 +48,18 @@ namespace gn.util{
             return gn.util.Geometry._outerBoundingClientRect(el).height;
         }
     }
+    class UUID {
+        static new() {
+            if(crypto && crypto.randomUUID) {
+                return crypto.randomUUID();
+            } else {
+                return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+                .replace(/[xy]/g, function (c) {
+                    const r = Math.random() * 16 | 0, 
+                        v = c == 'x' ? r : (r & 0x3 | 0x8);
+                    return v.toString(16);
+                });
+            }
+        }
+    }
 }
